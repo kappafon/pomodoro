b344f8dad0eacf4dae696a714d30c3bcc45f2689
kappafon <goran.rihter@gmail.com>
2019-11-23 11:42:13 +0100

    Updates

---
+++ precache-manifest.59b77fd93df52fc1bbeb17c3e9fbd681.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8b6cfc3b1f285c41737fde12ccc047a",
+    "url": "/kappafon/pomodoro.git/index.html"
+  },
+  {
+    "revision": "6d366db80414ce308693",
+    "url": "/kappafon/pomodoro.git/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "71c0a0c464c5cb408b75",
+    "url": "/kappafon/pomodoro.git/static/js/2.5866a7f1.chunk.js"
+  },
+  {
+    "revision": "6d366db80414ce308693",
+    "url": "/kappafon/pomodoro.git/static/js/main.f10e6ac0.chunk.js"
+  },
+  {
+    "revision": "802814438a3f62d152f1",
+    "url": "/kappafon/pomodoro.git/static/js/runtime-main.4cdfd18a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/kappafon/pomodoro.git/static/media/logo.25bf045c.svg"
+  }
+]);
